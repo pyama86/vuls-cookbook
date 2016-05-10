1d89435dd6305ff2c31a0f622d94861ce6d11d62
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2016-05-10 19:36:58 +0900

    fix template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -67,17 +67,24 @@ end
 #end
 
 
-template "/etc/init.d/go-cve-dictionary" do
-  source 'go-cve-dictionary.erb'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  variables :vars => {
-    'go_root' => node['golang']['root'],
-    'go_path' => "#{node['user']['home']}/go",
-    'user_home'=> node['user']['home']
-  }
+execute 'go-cve-dictionary server' do
+  cwd node['user']['home']
+  command "#{node['golang']['command']} run #{node['user']['home']}/go-cve-dictionary/main.go server -dbpath=#{node['user']['home']}/cve.sqlite3 >/dev/null 2>&1 &"
+  creates "#{node['user']['home']}/cve.sqlite3"
 end
+
+
+#template "/etc/init.d/go-cve-dictionary" do
+#  source 'go-cve-dictionary.erb'
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#  variables :vars => {
+#    'go_root' => node['golang']['root'],
+#    'go_path' => "#{node['user']['home']}/go",
+#    'user_home'=> node['user']['home']
+#  }
+#end
 
 execute 'go-cve-dictionary server' do
   command "/etc/init.d/go-cve-dictionary start"
