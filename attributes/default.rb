f1fb41de7a8a0403c72b0202f9ba45e97f57587f
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2016-07-06 04:02:29 +0900

    use glide

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,35 +1,20 @@
+default["setting"]["publish_ssh_pub_key"] = false
+
 default['package']['names'] = %w(wget vim git gcc sqlite)
 
 default['user']['name'] = 'ec2-user'
-default['user']['home'] = "/home/#{default['user']['name']}"
 
 default['golang']['version'] = '1.6'
 default['golang']['arch'] = 'amd64'
 default['golang']['root_dir'] = '/usr/local'
-default['golang']['root'] = "#{default['golang']['root_dir']}/go"
-default['golang']['command'] = "#{default['golang']['root']}/bin/go"
-default['golang']['src']['name'] = "go#{default['golang']['version']}.linux-#{default['golang']['arch']}.tar.gz"
-default['golang']['src']['url'] = "https://storage.googleapis.com/golang/#{default['golang']['src']['name']}"
 
-default['vuls']['go-cve-dictionary']['url'] = 'https://github.com/kotakanbe/go-cve-dictionary'
+default['vuls']['go-cve-dictionary']['path'] = 'github.com/kotakanbe/go-cve-dictionary'
 default['vuls']['go-cve-dictionary']['branch'] = 'master'
-default['vuls']['go-cve-dictionary']['imports'] = %w(
-  github.com/google/subcommands
-  github.com/kotakanbe/go-cve-dictionary/commands
-  github.com/mattn/go-sqlite3
-  golang.org/x/net/context
-)
-default['vuls']['go-cve-dictionary']['nvd']['start_year'] = '2011'
+default['vuls']['go-cve-dictionary']['nvd']['start_year'] = '2013'
 default['vuls']['go-cve-dictionary']['nvd']['end_year'] = '2016'
 
-default['vuls']['scanner']['url'] = 'https://github.com/future-architect/vuls.git'
+default['vuls']['scanner']['path'] = 'github.com/future-architect/vuls'
 default['vuls']['scanner']['branch'] = 'master'
-default['vuls']['scanner']['imports'] = %w(
-  golang.org/x/net/context
-  github.com/google/subcommands
-  github.com/mattn/go-sqlite3
-  github.com/future-architect/vuls/commands
-)
 default['vuls']['scanner']['server']['amazon'] = %w()
 default['vuls']['scanner']['server']['centos'] = %w()
 default['vuls']['scanner']['server']['ubuntu'] = %w()
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,20 +8,31 @@
 #
 #
 
+# set vars
+user_home = "/home/#{node['user']['name']}"
+go_root = "#{node['golang']['root_dir']}/go"
+go_path = "#{user_home}/go"
+go_bin = "#{go_path}/bin"
+go_cmd = "#{go_root}/bin/go"
+go_src_name = "go#{node['golang']['version']}.linux-#{node['golang']['arch']}.tar.gz"
+go_src_url = "https://storage.googleapis.com/golang/#{go_src_name}"
+go_cve_dictionary_abs_path = "#{go_path}/src/#{node['vuls']['go-cve-dictionary']['path']}"
+scanner_abs_path = "#{go_path}/src/#{node['vuls']['scanner']['path']}"
+
 node['package']['names'].each do |pack|
   package pack
 end
 
 execute 'create ssh keys' do
   user node['user']['name']
-  command "ssh-keygen -t rsa -b 4096 -N '' -f #{node['user']['home']}/.ssh/id_rsa"
-  not_if { File.exists?("#{node['user']['home']}/.ssh/id_rsa.pub") }
+  command "ssh-keygen -t rsa -b 4096 -N '' -f #{user_home}/.ssh/id_rsa"
+  not_if { File.exists?("#{user_home}/.ssh/id_rsa.pub") && ["setting"]["publish_ssh_pub_key"] }
 end
 
 execute 'publish id_rsa.pub' do
   user node['user']['name']
-  command "ruby -run -e httpd #{node['user']['home']}/.ssh/id_rsa.pub -p 1414 >/dev/null 2>&1 &"
-  only_if { File.exists?("#{node['user']['home']}/.ssh/id_rsa.pub") }
+  command "ruby -run -e httpd #{user_home}/.ssh/id_rsa.pub -p 1414 >/dev/null 2>&1 &"
+  only_if { File.exists?("#{user_home}/.ssh/id_rsa.pub") && node["setting"]["publish_ssh_pub_key"] }
 end
 
 template "/etc/profile.d/goenv.sh" do
@@ -30,26 +41,33 @@ template "/etc/profile.d/goenv.sh" do
   group 'root'
   mode '0755'
   variables :vars => {
-    'go_root' => node['golang']['root'],
-    'go_path' => "#{node['user']['home']}/go"
+    'go_root' => go_root,
+    'go_path' => go_path
   }
 end
 
 ruby_block "source_go_env" do
   block do
-    ENV['GOPATH'] = "#{node['user']['home']}/go"
+    ENV['GOPATH'] = go_path
     ENV['GOOS'] = 'linux'
     ENV['GOARCH'] = node['golang']['arch']
-    ENV['GOROOT'] = node['golang']['root']
+    ENV['GOROOT'] = go_root
+    ENV['GO15VENDOREXPERIMENT'] = "1"
   end
   action :run
 end
 
 execute 'install golang' do
-  command "wget #{node['golang']['src']['url']} -P #{node['user']['home']} &&
-           tar xzf #{node['user']['home']}/#{node['golang']['src']['name']} -C #{node['golang']['root_dir']}"
-  creates  node['golang']['root']
+  command "wget #{go_src_url} -P #{user_home} &&
+           tar xzf #{user_home}/#{go_src_name} -C #{node['golang']['root_dir']}"
+  creates  go_root
   notifies :create, "ruby_block[source_go_env]", :immediately
+end
+
+directory "#{user_home}/go" do
+  owner node['user']['name']
+  group node['user']['name']
+  mode '0755'
 end
 
 directory '/var/log/vuls' do
@@ -58,33 +76,52 @@ directory '/var/log/vuls' do
   mode '0700'
 end
 
-git "go-cve-dictionary" do
-  destination "#{node['user']['home']}/go-cve-dictionary"
-  repository node['vuls']['go-cve-dictionary']['url']
-  revision node['vuls']['go-cve-dictionary']['branch']
+execute "install glide" do
+  command "#{go_cmd} get github.com/Masterminds/glide"
+  user node['user']['name']
+  environment ({
+    'GOPATH' => go_path,
+    'GOROOT' => go_root
+  })
+end
+
+execute "git clone go-cve-dictionary" do
+  command "git clone http://#{node['vuls']["go-cve-dictionary"]['path']} -b #{node['vuls']['go-cve-dictionary']['branch']} #{go_cve_dictionary_abs_path}"
   user node['user']['name']
   group node['user']['name']
 end
 
-node['vuls']['go-cve-dictionary']['imports'].each do |pack|
-  execute "install #{pack} for go-cve-dictionary" do
-    command "#{node['golang']['command']} get #{pack}"
-    creates "#{node['golang']['root']}/src/#{pack}"
-  end
+execute "install package and build for go-cve-dictionary" do
+  cwd go_cve_dictionary_abs_path
+  command "#{go_bin}/glide install && #{go_cmd} build"
+  environment ({
+    'GOPATH' => go_path,
+    'GOROOT' => go_root,
+    'PATH' => "#{ENV['PATH']}:#{go_root}/bin:#{go_path}/bin"
+  })
+  user node['user']['name']
 end
 
-git 'vuls scanner' do
-  destination "#{node['user']['home']}/vuls"
-  repository node['vuls']['scanner']['url']
-  revision node['vuls']['scanner']['branch']
+execute "git clone scanner" do
+  command "git clone http://#{node['vuls']["scanner"]['path']} -b #{node['vuls']['scanner']['branch']} #{scanner_abs_path}"
   user node['user']['name']
   group node['user']['name']
 end
 
-node['vuls']['scanner']['imports'].each do |pack|
-  execute "install #{pack} for vuls scanner" do
-    command "#{node['golang']['command']} get #{pack}"
-    creates "#{node['golang']['root']}/src/#{pack}"
-  end
+execute "install package and build for scanner" do
+  cwd scanner_abs_path
+  command "#{go_bin}/glide install && #{go_cmd} build"
+  environment ({
+    'GOPATH' => go_path,
+    'GOROOT' => go_root,
+    'PATH' => "#{ENV['PATH']}:#{go_root}/bin:#{go_path}/bin"
+  })
+  user node['user']['name']
 end
 
+execute 'fetch NVD' do
+  cwd user_home
+  command "number=#{node['vuls']['go-cve-dictionary']['nvd']['start_year']};while [ \"$number\" -lt #{node['vuls']['go-cve-dictionary']['nvd']['end_year']} ]; do #{go_cve_dictionary_abs_path}/go-cve-dictionary fetchnvd -years $number; number=`expr $number + 1`; done"
+  user node['user']['name']
+  creates "#{user_home}/cve.sqlite3"
+end
